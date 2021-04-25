8bd7dad0f7d203c5a224283f15580df6d06c96f7
ElnurN <61333596+ElnurN@users.noreply.github.com>
2021-04-25 15:43:13 -0700

    Add files via upload

---
+++ TapMuhasib.az (half part)/js/MühasibTap.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(document).ready(function(){
+$(".hamburger").click(function(){
+    $(".hide_menu").toggle();
+});
+
+$(".fa-times").click(function(){
+    $(".hide_menu").hide();
+});
+
+$(".fa-file-image").click(function(){
+    $("#add_picture").click(); 
+})
+});
